7121b41979da5a5413821ec373ef1d71a7096031
Steven Jenkins <steven.jenkins@gmail.com>
2021-09-25 11:17:23 -0400

    Add support for /admin

--- crm/settings.py
+++ crm/settings.py
@@ -23,6 +23,7 @@ DEBUG = os.getenv("DEBUG")
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.messages",
--- crm/urls.py
+++ crm/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import include, path
 from drf_yasg.views import get_schema_view
@@ -38,6 +39,7 @@ urlpatterns = [
     url(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
+    path("admin/", admin.site.urls),
     path("api/", include("common.app_urls", namespace="common_urls")),
     path("logout/", views.LogoutView, {"next_page": "/login/"}, name="logout"),
 ]
